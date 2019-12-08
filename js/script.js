524ce9cf080199e48e1dd25718c36ad278f99738
Cak Lul Official Website <50877621+caklulofficialwebsite@users.noreply.github.com>
2019-12-08 07:50:57 +0700

    File Javascript
    Berisi tentang sintax Javascript

---
+++ js/script.js
@@ -0,0 +1,5 @@
+alert('Selamat datang di Cak Lul Official Website. Mohon maaf atas ketidaknyaman anda saat berjelajah di website kami \n Website ini masih dalam pengembangan. Ini adalah versi 2.2');
+
+// if (progress === true) {
+//     alert('terima kasih, Silahkan melanjutkan pengalaman Browsing di Website kami');
+// }
