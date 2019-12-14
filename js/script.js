ee0fa13b57ffe93e68d1637775c5fb2ca44e5715
Cak Lul Official Website <50877621+caklulofficialwebsite@users.noreply.github.com>
2019-12-14 12:06:15 +0700

    Update script.js
    mengganti beberapa syntax

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-alert('Selamat datang di Cak Lul Official Website. Mohon maaf atas ketidaknyaman anda saat berjelajah di website kami \n Website ini masih dalam pengembangan. Ini adalah versi 2.2');
+alert('Selamat datang di Cak Lul Official Website.\n\n\n\n Mohon maaf atas ketidaknyaman anda saat berjelajah di website kami \n\n Website ini masih dalam pengembangan.\n\n\n Ini adalah versi 2.2 \n\n');
 
 // if (progress === true) {
 //     alert('terima kasih, Silahkan melanjutkan pengalaman Browsing di Website kami');
