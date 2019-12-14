4d125220d4226e1e27d58811f9bada06215e0ab2
Cak Lul Official Website <50877621+caklulofficialwebsite@users.noreply.github.com>
2019-12-14 19:41:11 +0700

    Update script.js
    Menggunakan SweetAlert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,2 @@
-alert('Selamat datang di Cak Lul Official Website.\n\n\n\n Mohon maaf atas ketidaknyaman anda saat berjelajah di website kami \n\n Website ini masih dalam pengembangan.\n\n\n Ini adalah versi 2.2 \n\n');
-
-// if (progress === true) {
-//     alert('terima kasih, Silahkan melanjutkan pengalaman Browsing di Website kami');
-// }
+document.querySelector(".third")
+    swal("Selamat Datang di CLOW", "Mohon Maaf atas ketidaknyamanan anda!", "success");
