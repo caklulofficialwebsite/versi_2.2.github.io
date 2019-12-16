e698c69476fafe5dbb38352e012723c50f6fe929
Cak Lul Official Website <50877621+caklulofficialwebsite@users.noreply.github.com>
2019-12-16 09:43:31 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
 document.querySelector(".third")
-    swal("Selamat Datang di CLOW", "Mohon Maaf atas ketidaknyamanan anda!", "warning");
+    swal("Selamat Datang di CLOW", "Mohon Maaf atas ketidaknyamanan anda!", "error");
